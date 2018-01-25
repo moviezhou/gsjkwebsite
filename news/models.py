ee8b42de1848a474056c661adf8234179e697f43
moviezhou <moviezhou@gmail.com>
2018-01-26 03:32:21 +0800

    business domain management

--- news/models.py
+++ news/models.py
@@ -4,6 +4,12 @@ from django.shortcuts import render
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailadmin.edit_handlers import FieldPanel
+
+# from wagtail.wagtailcore.fields import StreamField
+# from wagtail.wagtailcore import blocks
+# from wagtail.wagtailadmin.edit_handlers import FieldPanel, StreamFieldPanel
+# from wagtail.wagtailimages.blocks import ImageChooserBlock
+
 from wagtail.wagtailsearch import index
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 
@@ -159,6 +165,39 @@ class EnterprisePage(Page):
         FieldPanel('enterprise_category'),
         FieldPanel('enterprise_name'),
         FieldPanel('enterprise_intro'),
-        FieldPanel('enterprise_logo'),
+        ImageChooserPanel('enterprise_logo'),
         FieldPanel('enterprise_website'),
     ]
+
+class BusinessDomain(Page):
+    class Meta:
+        verbose_name = "业务领域"
+
+    BUSINESS_CATEGORY = (
+        ("主金融", "主金融"),
+        ("类金融", "类金融"),
+        ("平台类", "平台类"),)
+
+    business_category = models.CharField(max_length=10, choices=BUSINESS_CATEGORY, default=1, verbose_name="业务领域")   
+    
+    business_name = models.CharField(max_length=60, verbose_name="业务名称")
+    business_icon = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        verbose_name="业务领域图标")
+    
+    # business_intro = StreamField([
+    #     ('heading', blocks.CharBlock(classname="full title")),
+    #     ('paragraph', blocks.RichTextBlock()),
+    #     ('image', ImageChooserBlock()),])
+
+    business_intro = RichTextField(blank=True, verbose_name="领域简介")
+
+    content_panels = Page.content_panels + [
+        FieldPanel('business_category'),
+        FieldPanel('business_name'),
+        ImageChooserPanel('business_icon'),
+        FieldPanel('business_intro'),]
