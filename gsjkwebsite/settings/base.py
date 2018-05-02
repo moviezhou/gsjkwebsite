8d40d638ef377646d71a29376b485263c60e0164
moviezhou <moviezhou@gmail.com>
2018-05-03 03:37:06 +0800

    fix invest company ani issue

--- gsjkwebsite/settings/base.py
+++ gsjkwebsite/settings/base.py
@@ -94,27 +94,27 @@ WSGI_APPLICATION = 'gsjkwebsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         # 'ENGINE': 'django.db.backends.sqlite3',
-#         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'gsjkwebsite',
-#         'USER': 'gsjkadmin',
-#         'PASSWORD': '00852',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
+DATABASES = {
+    'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3', 
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gsjkwebsite',
+        'USER': 'gsjkadmin',
+        'PASSWORD': '00852',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 # Database for Mac 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 
