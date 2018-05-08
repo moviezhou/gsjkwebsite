aed8c25c8617f992124505ed036be5b81ef0f8fd
moviezhou <moviezhou@gmail.com>
2018-05-09 03:03:26 +0800

    add idform commit email notification

--- gsjkwebsite/settings/dev.py
+++ gsjkwebsite/settings/dev.py
@@ -17,7 +17,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'moviezhou@gmail.com'
-EMAIL_HOST_PASSWORD = '1123581321fbnc'
+EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = 'moviezhou@gmail.com'
 
 
